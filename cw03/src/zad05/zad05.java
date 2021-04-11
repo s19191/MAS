aa6e50a956596b9451a414c0a002a36fce8998d6
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-04-12 01:58:46 +0200

    zad05, ale z ZonedDataTime, można też by było zrobić z LocalDate, albo LocalDateTime

--- cw03/src/zad05/zad05.java
+++ cw03/src/zad05/zad05.java
@@ -5,8 +5,8 @@ import java.time.*;
 public class zad05 {
     public static void main(String[] args) {
         Person person01 = new Person("Jan", "Kwasowski", Sex.MALE, ZonedDateTime.of(LocalDateTime.of(LocalDate.of(1998,9,23), LocalTime.MIDNIGHT), ZoneId.of("Europe/Warsaw")));
-        Person person02 = new Person("Jan", "Kwasowski", Sex.MALE, ZonedDateTime.of(LocalDateTime.of(LocalDate.of(1998,9,23), LocalTime.MIDNIGHT), ZoneId.of("Europe/Warsaw")));
-        Person person03 = new Person("Jan", "Kwasowski", Sex.MALE, ZonedDateTime.of(LocalDateTime.of(LocalDate.of(1998,9,23), LocalTime.MIDNIGHT), ZoneId.of("Europe/Warsaw")));
+        Person person02 = new Person("Ala", "Kowalska", Sex.FEMALE, ZonedDateTime.of(LocalDateTime.of(LocalDate.of(2002,1,3), LocalTime.MIDNIGHT), ZoneId.of("Europe/Warsaw")));
+        Person person03 = new Person("Alina", "Balladyna", Sex.FEMALE, ZonedDateTime.of(LocalDateTime.of(LocalDate.of(2020,4,15), LocalTime.MIDNIGHT), ZoneId.of("Europe/Warsaw")));
         System.out.println(Person.showAge(person01.caltulateAge()));
     }
 }
