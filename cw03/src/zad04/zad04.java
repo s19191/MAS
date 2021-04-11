a62d49e11040f7ddd37064b1c744401f0279a586
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-04-12 01:06:39 +0200

    zad03

--- cw03/src/zad04/zad04.java
+++ cw03/src/zad04/zad04.java
@@ -3,19 +3,39 @@ package zad04;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class zad04 {
     public static void main(String[] args) {
         program(4);
+        program(20);
     }
 
     static void program(int howManyLinesToShow) {
         try {
-            Stream<String> lines = Files.lines(Paths.get("src/zad04/text"));
-            //long howManyLines = lines.count();
-            for (int i = 0; i < howManyLinesToShow; i++) {
-
+            List<String> lines = Files.readAllLines(Paths.get("src/zad04/text"));
+            long howManyLines = lines.size();
+            if (howManyLinesToShow >= howManyLines) {
+                System.out.println(lines);
+            } else {
+                int counter = 0;
+                Random random = new Random();
+                List<String> choosen = new ArrayList<>();
+                while (howManyLinesToShow > 0) {
+                    if ((int)random.nextInt(2) % 2 == 0) {
+                        if (!choosen.contains(lines.get(counter))) {
+                            choosen.add(lines.get(counter));
+                            howManyLinesToShow--;
+                        }
+                    }
+                    counter++;
+                    if (counter == howManyLines) {
+                        counter = 0;
+                    }
+                }
+                System.out.println(choosen);
             }
         } catch (IOException e) {
             e.printStackTrace();
