5a61a915949c3c1366637dde9eca2ff9f0c0cc89
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-05-16 16:11:22 +0200

    wielodziedziczenie zrobione oraz przesłanianie z kalsy abstrakcyjnej

---
+++ mp03/src/Hybrid.java
@@ -0,0 +1,44 @@
+import java.time.LocalTime;
+
+public class Hybrid extends InternalCombustion implements IElectric {
+    Electric electric;
+
+    public Hybrid(String model, String mark, Double wage, Integer howManySeats, double tankCapacity, double averageCombustion, int batteryCapacity, double averagePowerConsumption) {
+        super(model, mark, wage, howManySeats, tankCapacity, averageCombustion, TypeOfGearbox.AUTOMATIC);
+        electric = new Electric(null, null, null, null, batteryCapacity, averagePowerConsumption, LocalTime.of(0,0));
+    }
+
+    public double calculateAverageMonthlyCost() {
+        return super.calculateAverageMonthlyCost() + electric.calculateAverageMonthlyCost();
+    }
+
+    @Override
+    public int getBatteryCapacity() {
+        return electric.getBatteryCapacity();
+    }
+
+    @Override
+    public void setBatteryCapacity(Integer batteryCapacity) throws NotNullException {
+        electric.setBatteryCapacity(batteryCapacity);
+    }
+
+    @Override
+    public double getAveragePowerConsumption() {
+        return electric.getAveragePowerConsumption();
+    }
+
+    @Override
+    public void setAveragePowerConsumption(Double averagePowerConsumption) throws NotNullException {
+        electric.setAveragePowerConsumption(averagePowerConsumption);
+    }
+
+    @Override
+    public LocalTime getAverageChargingTime() {
+        return electric.getAverageChargingTime();
+    }
+
+    @Override
+    public void setAverageChargingTime(LocalTime averageChargingTime) throws NotNullException {
+        electric.setAverageChargingTime(averageChargingTime);
+    }
+}
---
+++ mp03/src/TypeOfGearbox.java
@@ -0,0 +1,3 @@
+public enum TypeOfGearbox {
+    MANUAL, AUTOMATIC
+}
